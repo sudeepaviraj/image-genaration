c8170d24b2a343adeb9210d9234e13ddeb55de7d
Sudeep Virajith <virajithaabekoon@gmail.com>
2023-06-03 09:35:02 +0530

    Download function added

--- image.py
+++ image.py
@@ -1,31 +1,51 @@
-from PIL import Image,ImageDraw,ImageFont
+from PIL import Image, ImageDraw, ImageFont
 import os
 from io import BytesIO
 import base64
 from rembg import remove
+from youtubesearchpython import VideosSearch
+import yt_dlp
+import json
+import subprocess
 
-def create(text)->Image:
-    art = Image.new("RGBA",(512,512))
+
+def create(text) -> Image:
+    art = Image.new("RGBA", (512, 512))
     draw = ImageDraw.Draw(art)
     fnt = ImageFont.truetype("fonts/Holimount.otf", 180)
-    w, h = draw.textsize(text,font=fnt)
-    draw.text(((512-w)/2,(512-h)/2), text,font=fnt, fill=(255, 255, 255, 255))
+    w, h = draw.textsize(text, font=fnt)
+    draw.text(((512-w)/2, (512-h)/2), text,
+              font=fnt, fill=(255, 255, 255, 255))
     print(os.listdir("fonts"))
-    art.save("images/sign.png","png",lossless=True)
+    art.save("images/sign.png", "png", lossless=True)
     return "ok"
 
-def sticker(text)->Image:
-    art = Image.new("RGBA",(512,512))
+
+def sticker(text) -> Image:
+    art = Image.new("RGBA", (512, 512))
     draw = ImageDraw.Draw(art)
     fnt = ImageFont.truetype("fonts/Holimount.otf", 180)
-    w, h = draw.textsize(text,font=fnt)
-    draw.text(((512-w)/2,(512-h)/2), text,font=fnt, fill=(255, 255, 255, 255))
+    w, h = draw.textsize(text, font=fnt)
+    draw.text(((512-w)/2, (512-h)/2), text,
+              font=fnt, fill=(255, 255, 255, 255))
     print(os.listdir("fonts"))
-    art.save("images/sign.png","png",lossless=True)
+    art.save("images/sign.png", "png", lossless=True)
     return "ok"
 
-def no_bg(image)->Image:
+
+def no_bg(image) -> Image:
     art = Image.open(BytesIO(base64.b64decode(image)))
     nobg = remove(art)
-    nobg.save("images/nobg.png","PNG")
+    nobg.save("images/nobg.png", "PNG")
     return "ok"
+
+
+def music(title):
+    videosSearch = VideosSearch(title, limit=1)
+    link = videosSearch.result()["result"][0]["link"]
+    filename = subprocess.check_output(f'yt-dlp {link} -f ba --print filename -o music/output.%(ext)s')
+    filename = filename.decode("utf-8").strip()
+    os.system(f"yt-dlp {link} -f ba -o music/output.%(ext)s")
+    os.system(f"ffmpeg -y -i {filename} -b:a 192k music/output.ogg")
+    return "ok"
+music("prathihari")
